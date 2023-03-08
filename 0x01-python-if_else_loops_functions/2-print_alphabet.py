84414cf71659861cb6694a2e5673d9da56a7d6b2
timmoh-king <timothykariuki43@gmail.com>
2023-03-08 11:03:23 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-for alphabet in range (97,123):
-    print(chr(alphabet), end="")
+#for alphabet in range (97,123):
+    #print(chr(alphabet), end="")
+for alpha_letters in range(ord('a'), ord('z')+1):
+    print("{:c}".format(alpha_letters), end="")
