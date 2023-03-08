a9da43eac50be898a224c37234ab98ff64fa613e
timmoh-king <timothykariuki43@gmail.com>
2023-03-08 01:53:44 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#for alphabet in range (97,123):
-   # print(chr(alphabet), end="")
 for alpha_letters in range(ord('a'), ord('z')+1):
+    if alpha_letters == 'e' or alpha_letters == 'q':
+        continue
     print("{:c}".format(alpha_letters), end="")
