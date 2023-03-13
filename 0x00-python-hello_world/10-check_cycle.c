3cd3ced853edbb9baa6ab1cc1fed909bfdf33956
timmoh-king <timothykariuki43@gmail.com>
2023-03-13 04:45:23 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,8 +8,21 @@
  */
 int check_cycle(listint_t *list)
 {
-	if (list == NULL)
+	listint_t *turtle, *hare;
+
+	if (list == NULL || list->next == NULL)
 		return (0);
-	else
-		return (1);
+
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+	return (0);
 }
