2de807a7f469239e1c9797de92b869985278485c
timmoh-king <timothykariuki43@gmail.com>
2023-03-28 13:08:17 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     elements = 0
 
     for i in range(x):
-        
+
         try:
             print("{}".format(my_list[i]), end="")
             elements += 1
@@ -13,4 +13,4 @@ def safe_print_list(my_list=[], x=0):
             break
 
     print("")
-    return (elements)
+    return elements
