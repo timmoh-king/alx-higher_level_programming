6abd30a65d46e257315fe84951185f86e297708a
timmoh-king <timothykariuki43@gmail.com>
2023-04-01 03:37:44 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""write a class square that defines square"""
+
+
+class Square:
+    """define the square private attributes"""
+    def __init__(self, size):
+        self.__size = size
