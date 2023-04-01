e2d1bb14e42e6868961addc014cf1158e6fc5870
timmoh-king <timothykariuki43@gmail.com>
2023-04-01 12:34:41 -0700

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square by"""
+
+
+class Square:
+    """define the self attribute"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            """define the value attribute"""
+            self.__size = value
+
+    """define the method to calculate area"""
+    def area(self):
+        return (self.__size ** 2)
+
+    """define a function to print # on stdout"""
+    def my_print(self):
+        if self.__size == 0:
+            print("#")
