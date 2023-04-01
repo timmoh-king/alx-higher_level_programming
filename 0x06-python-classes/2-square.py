0e60991859a569186e46bcbf3b9ead56a1e13aba
timmoh-king <timothykariuki43@gmail.com>
2023-04-01 09:29:18 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,15 +6,9 @@
 class Square:
     """define the private atttributes"""
     def __init__(self, size=0):
-        self.__size = size
 
-    try:
-         isinstance(self.__size, int)
-    except TypeError:
-        raise TypeError("size must be an integer")
-
-    if self.__size < 0:
-        try:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
             raise ValueError("size must be >= 0")
-        except:
-            raise
+        self.__size = size
