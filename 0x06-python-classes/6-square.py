23809a828c8d62436b47b3732040526f1673753e
timmoh-king <timothykariuki43@gmail.com>
2023-04-03 05:09:04 -0700

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square by"""
+
+
+class Square:
+    """define the self attribute"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = value
+
+    """define the method to calculate area"""
+    def area(self):
+        return (self.__size ** 2)
+
+    """define a function to print # on stdout"""
+    def my_print(self):
+        for i in range(self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
