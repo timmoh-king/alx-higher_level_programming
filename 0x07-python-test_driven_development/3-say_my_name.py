0c436437a59d90cb59649a1080a5557062e69a04
timmoh-king <timothykariuki43@gmail.com>
2023-04-09 07:20:00 -0700

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Write a function that prints my name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        Write a function that prints My name is <first name> <last name>
+
+        Args:
+            first_name (str): 1st string
+            last_name (str): 2nd string
+
+        Raises:
+            TypeError: if name is not string
+
+        Return: My name is <first name> <last name>
+    """
+    if (not isinstance(first_name, str) or not isinstance(last_name, str)):
+        raise TypeError(
+                "first_name must be a string or last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
