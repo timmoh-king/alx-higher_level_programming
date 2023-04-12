bcbac2408fc9ba9246973e968ecc09f307a3f009
timmoh-king <timothykariuki43@gmail.com>
2023-04-12 08:00:33 -0700

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """
+        a function that writes a string to a text file
+
+        Args:
+            filename(str): name of the file
+            text(str): the string to be added
+        
+        Return:
+            the number of characters written
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
+        return f.tell()
