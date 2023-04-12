4c57859e40160ff5e5368bfbeb713ed5ac273eee
timmoh-king <timothykariuki43@gmail.com>
2023-04-12 07:35:35 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,5 @@ def read_file(filename=""):
         Args:
             filename(string): the name of the file
     """
-    with open(filename, encoding = 'utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
