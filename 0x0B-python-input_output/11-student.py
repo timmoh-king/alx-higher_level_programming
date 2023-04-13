120994887dc2e4f1eb0d9c06a4ccf238ae86fc38
timmoh-king <timothykariuki43@gmail.com>
2023-04-13 07:09:07 -0700

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""class Student that defines a student by"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """declare public attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {d: getattr(self, d) for d in attrs if hasattr(self, d)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """You can assume json will always be a dictionary"""
+        for k, v in json.items():
+            setattr(self, k, v)
