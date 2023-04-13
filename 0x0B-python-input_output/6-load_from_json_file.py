f320031d7c9e2cceaaf81774a88e0d418808c3d1
timmoh-king <timothykariuki43@gmail.com>
2023-04-13 02:07:35 -0700

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,4 +7,4 @@ import json
 def load_from_json_file(filename):
     """function that creates an Object from a JSON"""
     with open(filename) as f:
-        return json.loads(f)
+        return json.load(f)
