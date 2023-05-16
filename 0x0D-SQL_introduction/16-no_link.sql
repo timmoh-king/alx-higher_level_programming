7bc958b3c21270796962274bbf071a44063086f5
timmoh-king <timothykariuki43@gmail.com>
2023-05-16 16:58:35 +0300

    15-groups.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- a script that lists all records of the table second_table
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+-- The database name will be passed as an argument to the mysql command
+INSERT INTO `second_table`(id, name, score) VALUES(5, 'Aria', 18);
+INSERT INTO `second_table`(id, name, score) VALUES(6, 'Aria', 12);
