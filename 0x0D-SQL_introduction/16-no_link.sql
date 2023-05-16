de196cc5360f6bb0db8773d8f90b473f1b4cb48f
timmoh-king <timothykariuki43@gmail.com>
2023-05-16 17:02:11 +0300

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -3,9 +3,6 @@
 -- Results should display the score and the name (in this order)
 -- Records should be listed by descending score
 -- The database name will be passed as an argument to the mysql command
--- INSERT INTO `second_table`(id, name, score) VALUES(5, 'Aria', 18);
--- INSERT INTO `second_table`(id, name, score) VALUES(6, 'Aria', 12);
-
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
