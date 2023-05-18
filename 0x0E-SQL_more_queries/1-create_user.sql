b164e3e7a3d65ab61c92c8a30de8f8ff9a9299ab
timmoh-king <timothykariuki43@gmail.com>
2023-05-18 12:17:38 +0300

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- a script that creates the MySQL server user user_0d_1.
+-- If the user user_0d_1 already exists, your script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
