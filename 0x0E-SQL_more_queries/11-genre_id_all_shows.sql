68bde6520b40645e85f785ad134eed75f444b36a
timmoh-king <timothykariuki43@gmail.com>
2023-05-19 10:36:55 +0300

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- a script that lists all shows contained in the database hbtn_0d_tvshows.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+SELECT `s`.`title`, `g`.`genre_id`
+	FROM `tv_shows` AS `s`
+	LEFT JOIN `tv_show_genres` AS `g`
+		ON `s`.`id` = `g`.`show_id`
+		ORDER BY `s`.`title`, `g`.`genre_id` ASC
