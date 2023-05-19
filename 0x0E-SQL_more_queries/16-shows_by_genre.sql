092849813f253c5670af90d2138f88d9cca8063f
timmoh-king <timothykariuki43@gmail.com>
2023-05-19 12:58:06 +0300

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,9 +2,9 @@
 -- If a show doesn’t have a genre, display NULL in the genre column
 SELECT `s`.`title`, `t`.`name`
 	FROM `tv_shows` AS `s`
-	INNER JOIN `tv_show_genres` AS `g`
+	LEFT JOIN `tv_show_genres` AS `g`
 		ON  `s`.`id` = `g`.`show_id`
 
-		INNER JOIN `tv_genres` AS `t`
+		LEFT JOIN `tv_genres` AS `t`
 			ON `t`.`id` = `g`.`genre_id`
 	ORDER BY `s`.`title`, `t`.`name` ASC;
