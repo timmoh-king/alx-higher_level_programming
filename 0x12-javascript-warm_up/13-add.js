6849b53d0ae70ef638f416ff1d7d6470146c9d88
timmoh-king <timothykariuki43@gmail.com>
2023-06-13 21:08:23 +0300

    13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+ * Write a function that returns the addition of 2 integers
+ * The function must be visible from outside
+ */
+exports.add = function add (a, b) {
+  return (a + b);
+};
