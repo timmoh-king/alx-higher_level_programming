4df444990cfda8e7b1741470bfd789ea8caa6f94
timmoh-king <timothykariuki43@gmail.com>
2023-07-26 17:26:28 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,10 @@
  * Write a script that writes a string to a file
  */
 
-const fs = require('fs')
+const fs = require('fs');
 const file = process.argv[2];
 const data = process.argv[3];
 
 fs.writeFile(file, data, (err) => {
-	if (err) throw err;
-})
+  if (err) throw err;
+});
