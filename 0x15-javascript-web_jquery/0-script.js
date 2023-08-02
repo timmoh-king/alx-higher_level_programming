8234cc02a088b98ec29e180e9b2c63380716ed5e
timmoh-king <timothykariuki43@gmail.com>
2023-08-02 21:05:30 +0300

    0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/*
+a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+*/
+
+const textColor = document.querySelector('header');
+textColor.style.color = '#FF0000';
