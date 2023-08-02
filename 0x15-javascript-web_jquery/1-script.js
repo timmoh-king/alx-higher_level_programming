f271f878f4090411d991980194851f05713b6e21
timmoh-king <timothykariuki43@gmail.com>
2023-08-02 21:55:39 +0300

    1-script.js

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/*
+ * Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+ */
+const textColor = $('header');
+textColor.css('color', '#FF0000');
